46415da9e684e8cbd6de0e0c52f84286a729785d
QUAN LII <quanli422@pingan.com.cn>
2018-09-27 12:52:23 +0800

    alertToaster added

--- app/global/alertToaster.service.js
+++ app/global/alertToaster.service.js
@@ -4,8 +4,50 @@ export class AlertToasterService {
         this.Notification = Notification;
     }
 
-    popup(type, title, content) {
-        switch (type) {
+    popup(title, content) {
+        let that = this;
+        return {
+            success() {
+                return that.Notification(
+                    {
+                        replaceMessage: true,
+                        message: `<div class="alert alert-success alert-dismissible">
+                                <h4><i class="icon fa fa-check"></i>${title || 'Success!'}</h4>
+                                 ${content || ''}</div>`
+                    });
+            },
+
+            warning() {
+                return that.Notification(
+                    {
+                        replaceMessage: true,
+                        message: `<div class="alert alert-warning alert-dismissible">
+                                <h4><i class="icon fa fa-info"></i>${title || 'Warning!'}</h4>
+                                 ${content || ''}</div>`
+                    });
+            },
+
+            error() {
+                return that.Notification(
+                    {
+                        replaceMessage: true,
+                        message: `<div class="alert alert-danger alert-dismissible">
+                                <h4><i class="icon fa fa-close"></i>${title || 'Error!'}</h4>
+                                 ${content || ''}</div>`
+                    });
+            },
+
+            pending() {
+                return that.Notification(
+                    {
+                        delay: 99999999,
+                        message: `<div class="alert alert-info alert-dismissible">
+                                <h4><i class="icon fa fa-cog fa-spin"></i>${title || 'Error!'}</h4>
+                                 ${content || ''}</div>`
+                    });
+            }
+        };
+        /*switch (type) {
             case "success":
                 return this.Notification(
                     {
@@ -38,7 +80,7 @@ export class AlertToasterService {
                                 <h4><i class="icon fa fa-cog fa-spin"></i>${title || 'Error!'}</h4>
                                  ${content || ''}</div>`
                     });
-        }
+        }*/
     }
 
     dialog(type, title, content) {
