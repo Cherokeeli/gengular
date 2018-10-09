57a65e42987f2d821fef51e20708652e4ecc86ba
QUAN LII <quanli422@pingan.com.cn>
2018-10-09 15:06:55 +0800

    FIX login problem

--- app/main/login/login.component.js
+++ app/main/login/login.component.js
@@ -20,7 +20,10 @@ class LoginController {
         console.log(this);
         this.mainService.logIn(this.user).then(res => {
             console.log(res);
-            this.$state.go('dashboard', {reload: true});
+            // this.$state.go('dashboard', null, {reload: true});
+            // TODO 进入Dashboard之后ui框架的js不执行，暂时强制刷新重新执行这些js
+            window.location.href = '#/app/dashboard';
+            window.location.reload();
         }, err => {
             console.info('error:',err);
             this.logSuccess = false;
