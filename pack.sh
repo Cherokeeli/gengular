0a95b02eb88f8baf46c4b33cdf928062ad3dae2a
QUAN LII <quanli422@pingan.com.cn>
2018-10-17 13:19:25 +0800

    MOD pack shell

--- pack.sh
+++ pack.sh
@@ -1,3 +1,5 @@
+# 填需要的上传的git目录
 upload_path="/Users/quanli422/Projects/xuguangwu486_osh-oms"
 
-npm run build:prd && cp dist.zip $upload_path && git pull origin master && git add . && git commit -m 'new front pack' && git push origin master
+# build并执行git上传
+npm run build:prd && cap dist.zip $upload_path && cd $upload_path && git pull origin master && git add dist.zip && git commit -m 'new front pack' && git push origin master
